39f894b7ce14a29c75a248119ef0688100f5d3ea
pramodd12321 <pramodd12321@gmail.com>
2022-11-01 17:47:10 +0545

    Update settings.py

--- funolympicGame/settings.py
+++ funolympicGame/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9)8*2nx^dz#&dfw30vt*@!l@ctaz78@6!&lskrc8-jmb6nn70o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['funolympicgame.herokuapp.com']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
