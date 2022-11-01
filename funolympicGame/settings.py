967d27a9f0bdd68f8121ed5df43370f18c83645e
pramodd12321 <pramodd12321@gmail.com>
2022-11-01 16:03:43 +0545

    sdf

--- funolympicGame/settings.py
+++ funolympicGame/settings.py
@@ -237,6 +237,7 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
