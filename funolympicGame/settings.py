dbac1d66c966753055ecf489930a43c89df6364f
pramodd12321 <pramodd12321@gmail.com>
2022-11-01 17:43:33 +0545

    sdf

--- funolympicGame/settings.py
+++ funolympicGame/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9)8*2nx^dz#&dfw30vt*@!l@ctaz78@6!&lskrc8-jmb6nn70o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- olympic/forms.py
+++ olympic/forms.py
@@ -1,10 +1,8 @@
-from tkinter import Widget
 from typing_extensions import Required
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
 from .models import *
 
 class CreateUserForm(UserCreationForm):
